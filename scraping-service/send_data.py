4a5d8f52a01d17fde1ecbe69ece29067ee3d8ec1
gabref <gabrielfranzer@gmail.com>
2024-10-14 23:51:42 +0200

    refactor: add envs

--- scraping-service/send_data.py
+++ scraping-service/send_data.py
@@ -1,8 +1,13 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_data_to_receiver(product):
 
-    url = 'http://product-receiver-service:3002/api/product'
+    base_url = os.getenv('PRODUCT_RECEIVER_BASE_URL', 'http://product-receiver-service:3002')
+    url = f'{base_url}/api/product'
     
     try:
         response = requests.post(url, json=product)
