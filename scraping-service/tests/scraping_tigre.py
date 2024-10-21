2b15c04e28e6b409355243885074f41e83740d0f
Orobianco <giucon.mike@outlook.it>
2024-10-21 18:10:19 +0200

    working in progress

--- scraping-service/tests/scraping_tigre.py
+++ scraping-service/tests/scraping_tigre.py
@@ -1,15 +1,8 @@
-import requests
-import json
 import time
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-
-driver.get("https://oasitigre.it/it/negozi-volantini.html") 
-
-shop = BeautifulSoup(driver.page_source, "html.parser")
+# Declaration of dictionaries and lists 
 
 shop_info = {
 	"name" : "tigre",
@@ -17,35 +10,54 @@ shop_info = {
 	"working_hours": "2"
 }
 
+categories_url = [
+	"https://oasitigre.it/it/spesa/reparti/Frutta-e-Verdura/Frutta-Fresca.html",
+	"https://oasitigre.it/it/spesa/reparti/Frutta-e-Verdura/Frutta-Pronta-da-mangiare.html"
+]
+
+product_info = {
+	"name" : "start",
+	"img_url" : "start",
+	"description" : "start",
+	"new_price" : "start",
+	"old_price" : "start"
+}
+
+# Declaration of functions
+
+# def extract_product_info(driver, micro_cate):
+# 	n_cards = len(driver.find_elements(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div[{micro_cate}]/div/div[2]/div/div/div[1]/div'))
+# 	# print(n_cards)
+# 	for k in range(1, n_cards):
+# 		product_info.update(name = driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div[3]/div[2]/h4').text)
+# 		product_info.update(img_url = driver.find_element(By.XPATH, f'').text)
+# 		product_info.update(description = driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div[3]/div[3]/p').text)
+# 		product_info.update(new_price = driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div[4]/div[1]/div[2]/p').text)
+# 		product_info.update(old_price = driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div[4]/div[1]/div[1]/p').text)
+# 		print(product_info)
+
+driver = webdriver.Firefox()
+driver.get("https://oasitigre.it/it/negozi-volantini.html") 
+
 time.sleep(4)
 driver.find_element(By.ID, "CybotCookiebotDialogBodyButtonDecline").click()
+understan why it can't find the button
+driver.find_element(By.XPATH, '/html/body/main/div[2]/div[1]/div[3]/div[1]/div[1]/div[3]').click()
+time.sleep(3)
+driver.find_element(By.CLASS_NAME, 'button button--blue conferma').click()
 
-shop_info.update(name = (shop.find('p', class_ = 'nameShop').text))
-shop_info.update(street = (shop.find('p', class_ = 'streetShop').text))
-shop_info.update(working_hours = (shop.find('p', class_ = 'closingHoursShop').text))
+shop_info.update(name = driver.find_element(By.XPATH, f'/html/body/main/div[2]/div[1]/div[3]/div[8]/div[1]/div[2]/p[1]/b').text)
+shop_info.update(street = driver.find_element(By.XPATH, f'/html/body/main/div[2]/div[1]/div[3]/div[8]/div[1]/div[2]/p[2]').text)
+shop_info.update(working_hours = driver.find_element(By.XPATH, f'/html/body/main/div[2]/div[1]/div[3]/div[8]/div[1]/div[2]/div/p/b').text)
 
-print(shop_info)
-
-# categories_url = [
-# 	"https://oasitigre.it/it/spesa/reparti/Frutta-e-Verdura/Frutta-Fresca.html",
-# 	"https://oasitigre.it/it/spesa/reparti/Frutta-e-Verdura/Frutta-Pronta-da-mangiare.html"
-# ]
-
-driver.get("https://oasitigre.it/it/spesa/reparti/Frutta-e-Verdura/Frutta-Fresca.html")
-time.sleep(5)
-info = BeautifulSoup(driver.page_source, "html.parser")
-# n_cards = (info.find_element(By.Xpath, f'/html/body/main/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div[3]/div[3]/p'))
-# print(n_cards)
-n_cards = info.find_all('div', class_='swiper-slide product-card-no-p swiper-slide-active')
-tittle = n_cards[0].find('a', class_='product-link product-link-title').text
-img_url = n_cards[0].find('a', class_='product-link product-link-img')
-sub_title = n_cards[0].find('div', class_='productSubTitle').text
-new_price = n_cards[0].find('div', class_='oldPrice').text
-old_price = n_cards[0].find('div', class_='newPrice').text
-print(tittle)
-print(img_url)
-print(sub_title)
-print(new_price)
-print(old_price)
+for x in categories_url:
+	driver.get(x)
+	time.sleep(5)
+	micro_cate = len(driver.find_elements(By.XPATH, '/html/body/main/div[1]/div[2]/div[2]/div'))
+	for k in range(2, micro_cate):
+		n_cards = len(driver.find_elements(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div[{micro_cate}]/div/div[2]/div/div/div[1]/div'))
+		for n in range(1, n_cards):
+			product_info.update(name = driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div[1]/div[{n}]/div/div[3]/div[2]/h4').text)
+			print(product_info["name"])
 
 driver.close()
