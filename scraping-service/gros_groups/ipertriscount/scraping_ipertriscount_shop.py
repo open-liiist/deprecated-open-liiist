cf98071cbdb9890f91e8feb8f32ae2fde7f20fbf
Orobianco <giucon.mike@outlook.it>
2024-11-13 12:47:23 +0100

    more progress in the scraping for the gros shops group

--- scraping-service/gros_groups/ipertriscount/scraping_ipertriscount_shop.py
+++ scraping-service/gros_groups/ipertriscount/scraping_ipertriscount_shop.py
@@ -10,17 +10,22 @@ def get_html_from_url(url, headers):
 
 	try:
 		response = requests.get(url, headers = headers)
-		response.raise_for_status()
-		return response.text
-	except requests.exceptions.RequestException as e:
-		print(f"Error fetching URL: {e}")
-		return None
+		return response
+	except:
+		if response.status_code != 200:
+			print('Request failed:', response.status_code)
+			exit()
 
 if __name__ == "__main__":
 	url = "https://www.cedigros.com/insegne/itemlist/filter.html?category=26&moduleId=219&Itemid=701&abb249e6156b7eea4b28c92fb743caa0=1&format=raw"
 	headers = {"Accept": "*/*"}
-	html_content = get_html_from_url(url, headers)
+	response = get_html_from_url(url, headers = headers)
 
-	if html_content:
-		soup = BeautifulSoup(html_content, 'html.parser')
-		print(soup.preatti)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	# shop = soup.find_all('small')
+	# for address in shop:
+	# 	print(address.get_text())
+	city = soup.find_all('h5')
+	for city_name in city:
+		print(city_name.get_text())
+	
