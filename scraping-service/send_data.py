3df3be080408d19c044fc239e38bd7e3f78aae57
Orobianco <giucon.mike@outlook.it>
2024-11-16 11:48:31 +0100

    changes for the merge

--- scraping-service/send_data.py
+++ scraping-service/send_data.py
@@ -6,31 +6,15 @@ load_dotenv()
 
 def send_data_to_receiver(product):
 
-    base_url = os.getenv('PRODUCT_RECEIVER_BASE_URL', 'http://product-receiver-service:3002')
-    url = f'{base_url}/api/product'
-    
-    try:
-        response = requests.post(url, json=product)
-        if response.status_code == 201:
-            print(f"Product sent successfully: {product['name']}")
-        else:
-            print(f"Failed to send {product['name']}. Error: {response.status_code}")
+	base_url = os.getenv('PRODUCT_RECEIVER_BASE_URL', 'http://product-receiver-service:3002')
+	url = f'{base_url}/api/product'
 
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred while sending {product['name']}: {e}")
+	try:
+		response = requests.post(url, json=product)
+		if response.status_code == 201:
+			print(f"Product sent successfully: {product['name']}")
+		else:
+			print(f"Failed to send {product['name']}. Error: {response.status_code}")
 
-def send_shop_info_to_receiver(shop_data):
-
-    base_url = os.getenv('SHOP_RECEIVER_BASE_URL', 'http://shop-receiver-service:3001')
-    url = f'{base_url}/api/shop'
-
-    try:
-        response = requests.post(url, json=shop_data)
-        if response.status_code == 201:
-            print(f"Shop information sent successfully: {shop_data['name']}")
-        else:
-            print(f"Failed to send {shop_data['name']}. Error: {response.status_code}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred while sending {shop_data['name']}: {e}")
-
+	except requests.exceptions.RequestException as e:
+		print(f"An error occurred while sending {product['name']}: {e}")
