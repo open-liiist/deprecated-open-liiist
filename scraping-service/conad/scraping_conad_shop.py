123e5ef596366a9f90f30ac091a70342529b65fc
Orobianco <giucon.mike@outlook.it>
2024-11-20 19:29:37 +0100

    finish and optimize the scraping of conad shops

--- scraping-service/conad/scraping_conad_shop.py
+++ scraping-service/conad/scraping_conad_shop.py
@@ -3,8 +3,10 @@
 import requests
 import json
 
-def scraping_shop():
+def scraping_shops():
+	
 	# Declaration of all information that request.post needs to function correctly
+
 	url = "https://spesaonline.conad.it/api/ecommerce/it-it.stores.json"
 	body_data = {
 		"latitudine": 41.8967068,
@@ -15,20 +17,22 @@ def scraping_shop():
 	headers = {"Accept": "*/*"}
 
 	# Save the requests.post call in object containing information about the HTTP response
+
 	response = requests.post(url, json=body_data, headers=headers)
 
 	# Check for successful response in case not print and exit
+
 	if response.status_code != 200:
 		print(f"Error retrieving data: {response.status_code}")
 		print(response.text)
 		exit()
 
-	shop_list = []
 
 	# Parse the JSON response into a Python dictionary
 
 	shop_data = response.json()['data']['orderAndCollect']
 	all_shop = shop_data['pointOfServices']
+	shop_list = []
 
 	# Loop through each shop information in the response
 
@@ -39,17 +43,18 @@ def scraping_shop():
 			"lat": info_need['geoPoint']['latitude'],
 			"long": info_need['geoPoint']['longitude'],
 			"city": info_need['address']['town'],
-			"zip_code": info_need['address']['postalCode']
+			"zip_code": info_need['address']['postalCode'],
 		}
 
-		# if info_need['serviceHours'][1]:
-		# 	shop_info['working_hours'] = f"{info_need['serviceHours'][0]}{info_need['serviceHours'][1]}"
-		# else:
-		# 	shop_info['working_hours'] = info_need['serviceHours']
-		# print(shop_info)
+		if 'serviceHours' in info_need:
+			if len(info_need['serviceHours']) == 1:
+				shop_info['working_hours'] = info_need['serviceHours'][0]
+			else:
+				service_hours = " ".join(info_need['serviceHours'])
+				shop_info['working_hours'] = service_hours
 		shop_list.append(shop_info)
-	print(shop_list)
+	# print(shop_list)
 	# return(shop_list)
 
 if __name__ == "__main__":
-	scraping_shop()
+	scraping_shops()
